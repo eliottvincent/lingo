2e9f0069e843874a7aa5f20ac0bdd0ab8d775f81
Eliott Vincent <eliott.20100@gmail.com>
2017-05-17 12:32:00 +0200

    Basic tests

---
+++ src/test/java/com/eliottvincent/lingo/Controller/HistoryControllerTest.java
@@ -0,0 +1,10 @@
+package com.eliottvincent.lingo.Controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by eliottvct on 17/05/17.
+ */
+public class HistoryControllerTest extends TestCase {
+
+}
---
+++ src/test/java/com/eliottvincent/lingo/Model/SessionTest.java
@@ -0,0 +1,10 @@
+package com.eliottvincent.lingo.Model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by eliottvct on 17/05/17.
+ */
+public class SessionTest extends TestCase {
+
+}
