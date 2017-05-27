b860f496df7524c820c0aef1852050957e07eb98
Eliott Vincent <eliott.20100@gmail.com>
2017-05-27 11:55:35 +0200

    link between controller and fxml

--- src/main/java/com/eliottvincent/lingo/Controller/BasicController.java
+++ src/main/java/com/eliottvincent/lingo/Controller/BasicController.java
@@ -1,7 +1,18 @@
 package com.eliottvincent.lingo.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 /**
  * Created by eliottvct on 27/05/17.
  */
 public class BasicController {
+
+	@FXML private TextField name;
+
+	public void handleSaySup(ActionEvent actionEvent) {
+		System.out.printf("Sup %s!", name);
+	}
 }
