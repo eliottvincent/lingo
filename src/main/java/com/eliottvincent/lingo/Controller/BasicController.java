7e193c9d67d43393c3936e4f29da8ea5d2629d40
Eliott Vincent <eliott.20100@gmail.com>
2017-05-27 11:40:13 +0200

    moved sample.fxml

---
+++ src/main/java/com/eliottvincent/lingo/Controller/BasicController.java
@@ -0,0 +1,7 @@
+package com.eliottvincent.lingo.Controller;
+
+/**
+ * Created by eliottvct on 27/05/17.
+ */
+public class BasicController {
+}
--- src/main/java/com/eliottvincent/lingo/Main.java
+++ src/main/java/com/eliottvincent/lingo/Main.java
@@ -1,15 +1,9 @@
 package com.eliottvincent.lingo;
 
-import com.eliottvincent.lingo.Controller.ApplicationController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -24,9 +18,10 @@ public class Main extends Application
     }*/
 
 	@Override
-	public void start(Stage primaryStage) throws Exception{
-		//Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-		Group root = new Group();
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
+
+		/*Group root = new Group();
 
 		// Text
 		Text txt = new Text();
@@ -36,16 +31,27 @@ public class Main extends Application
 		// Text field
 		TextField txtField = new TextField();
 
+		// Label
+		Label lbl = new Label("Name:");
+
 		// Button
 		Button btn = new Button();
 		btn.setText("sup");
 		btn.setOnAction(evt -> System.out.printf("Sup %s!%n", txtField.getText()));
 
+		// GridPane
+		GridPane grid = new GridPane();
+		grid.setGridLinesVisible(true);
+		grid.add(lbl, 0, 0);
+		grid.add(txtField, 1, 0);
+		grid.setHgap(20);
+		grid.add(btn, 1, 1);
 
 		// Box
 		VBox box = new VBox();
-		box.getChildren().addAll(txt, txtField, btn);
+		box.getChildren().addAll(txt, grid);
 		root.getChildren().add(box);
+		*/
 
 		primaryStage.setTitle("Lingo");
 		primaryStage.setScene(new Scene(root, 300, 275));
