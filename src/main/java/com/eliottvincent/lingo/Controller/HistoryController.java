1b05bf4a0217b96799343a1d5baf03594530bffd
Eliott Vincent <eliott.20100@gmail.com>
2017-06-09 00:17:58 +0200

    History : udpated controller (db)

--- src/main/java/com/eliottvincent/lingo/Controller/HistoryController.java
+++ src/main/java/com/eliottvincent/lingo/Controller/HistoryController.java
@@ -1,7 +1,50 @@
 package com.eliottvincent.lingo.Controller;
 
+import com.eliottvincent.lingo.Model.History;
+
 /**
  * Created by eliottvct on 17/05/17.
  */
 public class HistoryController {
+
+	private DatabaseLayer databaseLayer = new DatabaseLayer();
+
+	public HistoryController() {
+
+	}
+
+	public History createHistory(Integer userId) {
+
+
+		History newHistory = new History();
+		newHistory.setUserId(userId);
+
+		// saving it in the database and getting the id of the statement
+		Integer id = this.saveHistory(newHistory);
+
+		// updating the History object with the id
+		newHistory.setId(id);
+
+		return newHistory;
+	}
+
+	/**
+	 *
+	 * @param history
+	 */
+	private void updateHistory(History history) {
+
+		this.databaseLayer.updateHistory(history);
+	}
+
+
+	/**
+	 *
+	 * @return
+	 * @param newHistory
+	 */
+	public Integer saveHistory(History newHistory) {
+
+		return this.databaseLayer.saveHistory(newHistory);
+	}
 }
