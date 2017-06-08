29fed99dfb17c40f8b783ba471aedebcc7a9d4ee
Eliott Vincent <eliott.20100@gmail.com>
2017-06-09 00:17:29 +0200

    Database : added controller and layer

--- src/main/java/com/eliottvincent/lingo/Controller/DatabaseController.java
+++ src/main/java/com/eliottvincent/lingo/Controller/DatabaseController.java
@@ -3,22 +3,23 @@ package com.eliottvincent.lingo.Controller;
 /**
  * Created by eliottvincent on 08/06/2017.
  */
-import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by eliottvincent on 06/06/2017.
  */
-public class QueryApp {
+public class DatabaseController {
 
 	private static Connection connection = null;
 
-	public QueryApp() {
+	public DatabaseController() {
 
 		loadDriver();
-		this.connection = getConnection(connection);
+		connection = getConnection(connection);
 	}
 
 	private static void loadDriver() {
@@ -29,7 +30,7 @@ public class QueryApp {
 		}
 	}
 
-	public static Connection getConnection(Connection connection) {
+	private static Connection getConnection(Connection connection) {
 
 		if (connection == null) {
 
@@ -54,7 +55,7 @@ public class QueryApp {
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-			/*if (query.startsWith("select") || query.startsWith("SELECT")) {
+			if (query.startsWith("select") || query.startsWith("SELECT")) {
 
 				ResultSet rs = statement.executeQuery(query);
 
@@ -117,11 +118,105 @@ public class QueryApp {
 				}
 
 
-			}*/
+			}
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public List executeSelectQuery(String query) {
+
+		List<Map<String, Object>> statementsList = new ArrayList<>();
+
+		System.out.println(query);
+
+		try {
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+			ResultSet rs = statement.executeQuery(query);
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+
+			while (rs.next()) {
+
+				Map<String, Object> statementMap = new HashMap<>();
+
+				for (int i = 1; i <= columnsNumber; i++) {
+
+					int columnType = rsmd.getColumnType(i);
+
+					switch (columnType) {
+						case Types.VARCHAR:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getString(i));
+							break;
+						case Types.NULL:
+							statementMap.put(rsmd.getColumnLabel(i), "null");
+							break;
+						case Types.CHAR:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getString(i));
+							break;
+						case Types.TIMESTAMP:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getTimestamp(i));
+							break;
+						case Types.DOUBLE:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getDouble(i));
+							break;
+						case Types.INTEGER:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getString(i));
+							break;
+						case Types.SMALLINT:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getInt(i));
+							break;
+						case Types.DECIMAL:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getBigDecimal(i));
+							break;
+						default:
+							statementMap.put(rsmd.getColumnLabel(i), rs.getString(i));
+							break;
+					}
+				}
+
+				statementsList.add(statementMap);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return statementsList;
+	}
+
+	Integer executeCreateQuery(String query) {
+
+		Integer insertId = 0;
+
+		PreparedStatement pstmt = null;
+
+		try {
+			pstmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			pstmt.executeUpdate();
+
+			ResultSet keys = pstmt.getGeneratedKeys();
+			keys.next();
+			insertId = keys.getInt(1);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return insertId;
+
+	}
+
+	public void excuteInsertQuery(String query) {
+
+	}
+
+	public void executeUpdateQuery(String query) {
+
 	}
 
 }
---
+++ src/main/java/com/eliottvincent/lingo/Controller/DatabaseLayer.java
@@ -0,0 +1,96 @@
+package com.eliottvincent.lingo.Controller;
+
+import com.eliottvincent.lingo.Model.History;
+import com.eliottvincent.lingo.Model.User;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eliottvincent on 08/06/2017.
+ */
+class DatabaseLayer {
+
+	private DatabaseController databaseController = new DatabaseController();
+
+	public DatabaseLayer() {
+
+	}
+
+	/**
+	 *
+	 * @param newUser
+	 * @return
+	 */
+	Integer saveUser(User newUser) {
+
+		String query = "INSERT INTO Users (username, password, birthdate, gender, language) " +
+			"VALUES (" +
+			"'"		+	newUser.getUserName() 	+	 "', " 	+
+			"'"		+	newUser.getPassword() 	+	 "', "	+
+			"'"		+	newUser.getBirthdate() 	+	 "', " 	+
+			"'"		+	newUser.getGender() 	+	 "', " 	+
+			"'"		+	newUser.getLanguage() 	+	 "')";
+
+		return databaseController.executeCreateQuery(query);
+	}
+
+	Integer saveHistory(History newHistory) {
+
+		String query = "INSERT INTO Histories (user_id) " +
+			"VALUES (" +
+			"'" + newHistory.getUserId() + "'" +
+			")";
+
+		return databaseController.executeCreateQuery(query);
+	}
+
+	void updateUser(User user) {
+
+		String query = "UPDATE Users " +
+			"SET username = '" + user.getUserName() + "', " +
+			"password = '" + user.getPassword() + "', " +
+			"birthdate = '" + user.getBirthdate() + "', " +
+			"gender = '" + user.getGender() + "', " +
+			"language = '" + user.getLanguage() + "' " +
+			"WHERE id = " + user.getId() + "";
+		databaseController.executeUpdateQuery(query);
+	}
+
+	void updateHistory(History history) {
+
+		String query = "UPDATE Histories " +
+			"SET id = '" + history.getId() + "', " +
+			"SET user_id = '" + history.getUserId() + "' " +
+				"WHERE id = " + history.getId() + "";
+
+		databaseController.executeUpdateQuery(query);
+	}
+
+	User searchUser(String username, String password) {
+
+		User tmpUser = new User();
+
+		String query = "SELECT * FROM Users " +
+			"WHERE username LIKE '" + username + "' " +
+			"AND password LIKE '" + password + "'";
+
+		List usersList = databaseController.executeSelectQuery(query);
+
+		tmpUser.setId(1);
+
+		return tmpUser;
+	}
+
+	boolean usernameAlreadyExist(String username) {
+
+		String query = "SELECT * FROM Users " +
+			"WHERE username LIKE '" + username + "'";
+
+		List usersList = databaseController.executeSelectQuery(query);
+
+		// we return true if the list isn't null and isn't empty
+		return usersList != null && !usersList.isEmpty();
+	}
+}
