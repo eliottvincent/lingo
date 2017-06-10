9a16aa791c4bb39f612194d816c1f6ef4945647c
Eliott Vincent <eliott.20100@gmail.com>
2017-06-10 16:41:20 +0200

    [General] Added ConverterHelper back

---
+++ src/main/java/com/eliottvincent/lingo/ConverterHelper.java
@@ -0,0 +1,163 @@
+package com.eliottvincent.lingo;
+
+/**
+ * Created by eliottvincent on 09/06/2017.
+ */
+
+import com.eliottvincent.lingo.Data.Language;
+import com.eliottvincent.lingo.Data.Gender;
+import com.eliottvincent.lingo.Model.History;
+import com.eliottvincent.lingo.Model.User;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+public class ConverterHelper {
+
+
+	/**
+	 *
+	 * @param language the language to be converted
+	 * @return
+	 */
+	public static Language stringToLanguage(String language) {
+
+		return Language.valueOf(language);
+	}
+
+	/**
+	 *
+	 * @param s the string to be converted
+	 * @return
+	 */
+	public static int stringToInteger(String s) {
+
+		return Integer.parseInt(s);
+	}
+
+	/**
+	 *
+	 * @param gender the gender to be converted
+	 * @return
+	 */
+	public static Gender stringToGender(String gender) {
+
+		return Gender.valueOf(gender);
+	}
+
+	/**
+	 *
+	 * @param language the language to be converted
+	 * @return
+	 */
+	private static String languageToString(Language language) {
+
+		return language.toString();
+	}
+
+	/**
+	 *
+	 * @param gender the gender to be converted
+	 * @return
+	 */
+	private static String genderToString(Gender gender) {
+
+		return gender.toString();
+	}
+
+	/**
+	 *
+	 * @param i the integer to be converted
+	 * @return
+	 */
+	private static String integerToString(Integer i) {
+
+		return i.toString();
+	}
+
+	/**
+	 *
+	 * @param history
+	 * @return
+	 */
+	private static String historyToString(History history) {
+
+		return ConverterHelper.integerToString(history.getId());
+	}
+
+	/**
+	 *
+	 * @param user the user to be converted
+	 * @return
+	 */
+	public static String[] userToStringArray(User user) {
+
+		// before converting it to an array, we need to find the id of
+		return new String[]{
+			user.getUsername(),
+			user.getPassword(),
+//			ConverterHelper.integerToString(user.getBirthdate()),
+			ConverterHelper.genderToString(user.getGender()),
+			ConverterHelper.languageToString(user.getLanguage()),
+			ConverterHelper.historyToString(user.getHistory()),
+		};
+	}
+
+	/**
+	 *
+	 * @param birthdateString
+	 * @return
+	 */
+	public static Date stringToDate(String birthdateString) {
+
+
+		String str = "01/01/2015";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		LocalDate dateTime = LocalDate.parse(str, formatter);
+
+		return java.sql.Date.valueOf(dateTime);
+	}
+
+	/**
+	 *
+	 * @param localDate
+	 * @return
+	 */
+	public static Date localeDateToDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	/**
+	 *
+	 * @param localDateTime
+	 * @return
+	 */
+	public static Date localeDateTimeToDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	/**
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static LocalDate dateToLocalDate(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	/**
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static LocalDateTime dateToLocalDateTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+}
