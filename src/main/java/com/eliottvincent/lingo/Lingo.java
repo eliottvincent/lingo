81e6e392bca84db6298ce4874d472bf6b3067b0b
Eliott Vincent <eliott.20100@gmail.com>
2017-06-18 00:17:14 +0200

    [General] added javadoc for Lingo class

--- src/main/java/com/eliottvincent/lingo/Lingo.java
+++ src/main/java/com/eliottvincent/lingo/Lingo.java
@@ -1,7 +1,7 @@
 package com.eliottvincent.lingo;
 
-import com.eliottvincent.lingo.ViewController.HomeViewController;
 import com.eliottvincent.lingo.ViewController.LoginViewController;
+
 import com.jfoenix.controls.JFXDecorator;
 import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.container.DefaultFlowContainer;
@@ -14,52 +14,69 @@ import javafx.stage.Stage;
 
 
 /**
- * Hello world!
+ * <b>Lingo is the main class of the application.</b>
  *
+ * @author eliottvincent
  */
 public class Lingo extends Application {
 
+
+	//================================================================================
+	// DataFX elements
+	//================================================================================
+
 	@FXMLViewFlowContext
-	public ViewFlowContext flowContext;
+	private ViewFlowContext flowContext;
+
 
 	//================================================================================
 	// Main and initialization
 	//================================================================================
 
 	/**
+	 * start() is responsible of starting the application
 	 *
-	 * @param stage
+	 * @param stage the primary stage of the Application
 	 */
 	@Override
 	public void start(Stage stage) throws Exception {
 
-
+		// creating a new Flow linked to the LoginViewController
 		Flow flow = new Flow(LoginViewController.class);
-
 		DefaultFlowContainer container = new DefaultFlowContainer();
+
+		// creating a new ViewFlowContext
+		// so we can save some data between flows
 		flowContext = new ViewFlowContext();
+
+		// registering the stage in the context
 		flowContext.register("Stage", stage);
-		flowContext.register("myvalue", "wahoueeee");
 		flow.createHandler(flowContext).start(container);
 
+		// creating a decorator
 		JFXDecorator decorator = new JFXDecorator(stage, container.getView());
 		decorator.setCustomMaximize(true);
 		Scene scene = new Scene(decorator, 1500, 750);
-		final ObservableList<String> stylesheets = scene.getStylesheets();
 
+		// adding the stylesheets to the application
+		final ObservableList<String> stylesheets = scene.getStylesheets();
 		stylesheets.addAll(
 			Lingo.class.getResource("/css/fonts.css").toExternalForm(),
 			Lingo.class.getResource("/css/home.css").toExternalForm()
 		);
+
+		// setting sizes
 		stage.setMinWidth(1500);
 		stage.setMinHeight(750);
 		stage.setScene(scene);
+
+		// showing the application
 		stage.show();
 	}
 
 	/**
 	 *
-	 * @param args
+	 * @param args the arguments passed to the application, if any
 	 */
 	public static void main(String[] args) {
 
