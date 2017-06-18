3d50d1d584161cd5e990fb8c3eb2659bc66b4436
Eliott Vincent <eliott.20100@gmail.com>
2017-06-18 13:41:08 +0200

    [General] javadoc ActionController

--- src/main/java/com/eliottvincent/lingo/Controller/ActionController.java
+++ src/main/java/com/eliottvincent/lingo/Controller/ActionController.java
@@ -12,7 +12,11 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by eliottvincent on 10/06/2017.
+ * <b>ActionController is the class responsible for creating and retrieving user actions.</b>
+ *
+ * @see Action
+ *
+ * @author eliottvincent
  */
 public class ActionController {
 
@@ -28,8 +32,9 @@ public class ActionController {
 	// Constructor
 	//================================================================================
 
+
 	/**
-	 *
+	 * The default constructor for an ActionController.
 	 */
 	public ActionController() {
 
@@ -41,15 +46,20 @@ public class ActionController {
 	//================================================================================
 
 	/**
+	 * getAction() is the method used to retrieve a specific Action.
 	 *
-	 * @param actionId
-	 * @return
+	 * @param actionId the id of the action to retrieve.
+	 * @return the retrieved action
+	 *
+	 * @see DatabaseController
 	 */
 	Action getAction(Integer actionId) {
 
+		// preparing the query
 		String query =	"SELECT * FROM Actions "	+
 						"WHERE id LIKE '" 			+	actionId	+	"'";
 
+		// executing the query
 		List<Map<String, Object>> actionsList = databaseController.executeSelectQuery(query);
 
 		// we should have only 1 statement
@@ -74,19 +84,29 @@ public class ActionController {
 	}
 
 	/**
-	 * used to return a list of all actions with the same session_id in db
-	 * @param sessionId
-	 * @return
+	 * getActions() is the method used to return a list of all actions with the same sessionId
+	 *
+	 * @param sessionId the id of the session containing the actions to retrieve
+	 * @return a list of actions
+	 *
+	 * @see DatabaseController
 	 */
 	List<Action> getActions(Integer sessionId) {
 
+		// preparing the query
 		String query = 	"SELECT * FROM Actions "	+
 						"WHERE session_id LIKE '"	+	sessionId	+	"'";
 
+		// executing the query
 		List<Map<String, Object>> actionsList = databaseController.executeSelectQuery(query);
+
+		// creating a list to host the retrieved actions
 		List<Action> actions = new ArrayList<Action>();
 
+		// for each action in actionsList...
 		for (Map<String, Object> actionMap : actionsList) {
+
+			// we need to convert it to a plain Action object
 			Action tmpAction = new Action();
 			tmpAction.setId(ConverterHelper.stringToInteger((String) actionMap.get("id")));
 			tmpAction.setSessionId(ConverterHelper.stringToInteger((String) actionMap.get("session_id")));
@@ -95,6 +115,7 @@ public class ActionController {
 			tmpAction.setData((String) actionMap.get("data"));
 			tmpAction.setExerciceId((String) actionMap.get("exercice_id"));
 
+			// once the Action object has been created and filled, we add it to the actions List
 			actions.add(tmpAction);
 		}
 
@@ -107,42 +128,50 @@ public class ActionController {
 	//================================================================================
 
 	/**
+	 * createNewAction() is the method responsible for creating a new Action object.
 	 *
-	 * @param user
-	 * @return
+	 * @param user the user who performed the action.
+	 * @param type the type of the action.
+	 * @param date the date on which the action was performed.
+	 * @param exerciceId the id of the exercise concerned by the action, if any.
+	 * @param data the data concerned by the action, if any.
+	 * @return the id of the database statement for the created action.
 	 */
 	public Integer createNewAction(User user, ActionType type, Date date, String exerciceId, String data) {
 
 		if (user != null) {
 
-			UserController userController = new UserController();
-			Session lastSession = userController.getLastSession(user);
-
+			// creating the Action object
 			Action action = new Action();
-
 			action.setType(type);
 			action.setDate(date);
 			action.setExerciceId(exerciceId);
 			action.setData(data);
 
+			// getting the last session of the user who performed the action
+			UserController userController = new UserController();
+			Session lastSession = userController.getLastSession(user);
 
-			// if the user's last session doesn't contain any "logout"
-			// we can continue to add actions to it
+			// if the user's last session doesn't contain any LOGOUT action
 			if (!containsLogout(lastSession.getActions())) {
 
+				// we can continue to add actions to his last session
+				// adding the id of the last session to the action
 				action.setSessionId(lastSession.getId());
 			}
 
 			// else we have to create a new session
 			else {
 
+				// creating a new Session for the user
 				SessionController sessionController = new SessionController();
 				Integer sessionId = sessionController.createNewSession(user.getHistory().getId());
 
+				// adding the id of the created session to the action
 				action.setSessionId(sessionId);
-
 			}
 
+			// calling the saveAction() method
 			return this.saveAction(action);
 		}
 		else {
@@ -152,12 +181,16 @@ public class ActionController {
 	}
 
 	/**
+	 * saveAction() is the method responsible for saving an Action object in the database.
 	 *
-	 * @param action
-	 * @return
+	 * @param action the Action object to save.
+	 * @return the id of the database statement for the created action.
+	 *
+	 * @see DatabaseController
 	 */
 	private Integer saveAction(Action action) {
 
+		// preparing the query
 		String query = 	"INSERT INTO Actions (session_id, type, date, data, exercice_id) " +
 						"VALUES (" 	+
 						"'"			+	action.getSessionId()	+ 	"', " +
@@ -166,16 +199,18 @@ public class ActionController {
 						"'"			+	action.getExerciceId()	+ 	"', " +
 						"'"			+	action.getData()		+ 	"')";
 
+		// executing the query thanks to the DatabaseController
 		return databaseController.executeInsertQuery(query);
 	}
 
 
 	/**
+	 * containsLogout() is the method responsible for checking if a session contains a LOGOUT action.
 	 *
-	 * @param list
-	 * @return
+	 * @param list the list of actions to check in.
+	 * @return true or false, according to the result.
 	 */
-	public boolean containsLogout(final List<Action> list) {
+	private boolean containsLogout(final List<Action> list) {
 
 		return list.stream().anyMatch(o -> o.getType().equals(ActionType.LOGOUT));
 	}
