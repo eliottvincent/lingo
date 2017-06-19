b2ac42b7c913936878b16f810e4033b79f84e40a
Eliott Vincent <eliott.20100@gmail.com>
2017-06-19 02:15:27 +0200

    [General] added javadoc for LoginViewController

--- src/main/java/com/eliottvincent/lingo/ViewController/LoginViewController.java
+++ src/main/java/com/eliottvincent/lingo/ViewController/LoginViewController.java
@@ -23,7 +23,6 @@ import io.datafx.controller.flow.context.ViewFlowContext;
 import io.datafx.controller.util.VetoException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -33,9 +32,10 @@ import javax.annotation.PostConstruct;
 import java.util.Date;
 
 /**
- * Created by eliottvct on 27/05/17.
+ * <b>LoginViewController is the class responsible for the Login view.</b>
+ *
+ * @author eliottvincent
  */
-
 @ViewController("/fxml/login.fxml")
 public class LoginViewController {
 
@@ -92,35 +92,34 @@ public class LoginViewController {
 
 	private ActionController actionController;
 
-	private String statusText;
-
-	private static final String FX_LABEL_FLOAT_TRUE = "-fx-label-float:true;";
-
-	private static final String EM1 = "1em";
-
-	private static final String ERROR = "error";
+	private UserController userController;
 
 	//================================================================================
 	// Constructor and initialization
 	//================================================================================
 
 	/**
-	 *
+	 * The default constructor for the LoginViewController.
 	 */
 	public LoginViewController() {
 
 		this.actionController = new ActionController();
+
+		this.userController = new UserController();
 	}
 
-
+	/**
+	 * the init() method is responsible for doing the necessary initialization of some components.
+	 * By adding the @PostConstruct annotation to the method, the DataFX flow container will call this method once all injectable values of the controller instance are injected.
+	 */
 	@PostConstruct
 	public void init() {
 
-		// the default focus is on the first text area
+		// by default, the initial focus is on the first text area
 		// we use this Runnable (encapsulated in a lambda function) to focus on the container
 		Platform.runLater(() -> container.requestFocus());
 
-		// handling key presses thanks to this lambda function 💪
+		// handling key presses thanks to this lambda function
 		container.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
 
 			// if the ENTER key was pressed
@@ -134,14 +133,22 @@ public class LoginViewController {
             }
         });
 
+		this.initializeFields();
+	}
+
+	/**
+	 * the initializeFields() method is responsible for initializing the credentials fields, especially their validators.
+	 */
+	private void initializeFields() {
+
 		RequiredFieldValidator validator = new RequiredFieldValidator();
 
-		usernameTextField.setStyle(FX_LABEL_FLOAT_TRUE);
+		usernameTextField.setStyle("-fx-label-float:true;");
 		validator.setMessage("Input required");
 		validator.setIcon(GlyphsBuilder.create(FontAwesomeIconView.class)
 			.glyph(FontAwesomeIcon.WARNING)
-			.size(EM1)
-			.styleClass(ERROR)
+			.size("1em")
+			.styleClass("error")
 			.build());
 		usernameTextField.getValidators().add(validator);
 		usernameTextField.focusedProperty().addListener((o, oldVal, newVal) -> {
@@ -151,12 +158,12 @@ public class LoginViewController {
 		});
 
 		validator = new RequiredFieldValidator();
-		passwordField.setStyle(FX_LABEL_FLOAT_TRUE);
+		passwordField.setStyle("-fx-label-float:true;");
 		validator.setMessage("Password can't be empty");
 		validator.setIcon(GlyphsBuilder.create(FontAwesomeIconView.class)
 			.glyph(FontAwesomeIcon.WARNING)
-			.size(EM1)
-			.styleClass(ERROR)
+			.size("1em")
+			.styleClass("error")
 			.build());
 		passwordField.getValidators().add(validator);
 		passwordField.focusedProperty().addListener((o, oldVal, newVal) -> {
@@ -164,22 +171,18 @@ public class LoginViewController {
 				passwordField.validate();
 			}
 		});
-
-		buttonsVBox.setSpacing(30);
-		buttonsVBox.setStyle("-fx-padding: 50px 0 0 0; -fx-border-insets: 50px 0 0 0; -fx-background-insets: 50px 0 0 0;");
-
-
 	}
 
 	/**
-	 *
+	 * the loginAction() method is responsible handling the user login
 	 */
 	@ActionMethod("loginAction")
 	private void loginAction() {
 
-		if (usernameTextField.getText().equals("admin") &&
-			passwordField.getText().equals("admin")) {
+		// if the credentials are "admin" and "admin"
+		if (usernameTextField.getText().equals("admin") && passwordField.getText().equals("admin")) {
 
+			// we show the Admin View
 			try {
 				this.navigateToAdmin();
 			} catch (VetoException | FlowException e) {
@@ -187,19 +190,21 @@ public class LoginViewController {
 			}
 
 		}
+
 		else {
 
-			UserController userController = new UserController();
-			User tmpUser = userController.getUserByCredentials(usernameTextField.getText(), passwordField.getText());
+			User tmpUser = this.userController.getUserByCredentials(usernameTextField.getText(), passwordField.getText());
 
-			// user does exist
+			// if the user has been found by the getUserByCredentials() method
 			if (tmpUser != null) {
 
-				// saving the action
+				// we create a "LOGIN" action
 				this.actionController.createNewAction(tmpUser, ActionType.LOGIN, new Date(), null, null);
 
-				// saving the User object in context
+				// we save the User object in the ViewFlowContext
 				flowContext.register("user", tmpUser);
+
+				// we show the Home View
 				try {
 					this.navigateToHome();
 				} catch (VetoException | FlowException e) {
@@ -210,28 +215,30 @@ public class LoginViewController {
 
 			// user doesn't exist
 			else {
+
 				statusLabel.setText("Incorrect username or password");
 			}
 		}
 	}
 
 	/**
+	 * the navigateToHome() method is responsible of opening the Home View.
 	 *
-	 * @throws VetoException
-	 * @throws FlowException
+	 * @throws VetoException the VetoException to throw, if any.
+	 * @throws FlowException the FlowException to throw, if any.
 	 */
-	void navigateToHome() throws VetoException, FlowException {
+	private void navigateToHome() throws VetoException, FlowException {
 
 		actionHandler.navigate(HomeViewController.class);
 	}
 
-
 	/**
+	 * the navigateToAdmin() method is responsible of opening the Admin View.
 	 *
-	 * @throws VetoException
-	 * @throws FlowException
+	 * @throws VetoException the VetoException to throw, if any.
+	 * @throws FlowException the FlowException to throw, if any.
 	 */
-	void navigateToAdmin() throws VetoException, FlowException {
+	private void navigateToAdmin() throws VetoException, FlowException {
 
 		actionHandler.navigate(AdminViewController.class);
 	}
